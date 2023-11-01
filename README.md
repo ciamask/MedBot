dce0202a356ea6ac51cc5b75d03b18551c434b52
Shreeya Maskey <maskey.shriya@gmail.com>
2023-10-31 18:45:15 -0600

    final chatbot

--- README.md
+++
@@ -1,2 +0,0 @@
-# wikibot
-# chatbot-disease-prediction
--- interface/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.views.generic.base import TemplateView
-from . import views
-
-urlpatterns=[
-    url(r'^home',TemplateView.as_view(template_name='index.html'),name='index'),
-    url(r'^python/$',views.exe,name='exe'),
-]
